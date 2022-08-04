27c6a41a7e53a7c4d9c19bf49ea751ddbcf8a39e
Marcelo914 <marcelosouza222244@gmail.com>
2022-08-04 19:48:29 -0300

    media das notas

---
+++ medias.py
@@ -0,0 +1,14 @@
+from functools import total_ordering
+
+
+primeiraNota = input("Digite sua primeira nota: ")
+segundaNota = input("Digite sua segunda nota: ")
+terceiraNota = input("Digite sua terceira nota: ")
+quartaNota = input("Digite sua terceira nota: ")
+primeiraNota = int(primeiraNota)
+segundaNota = int(segundaNota)
+terceiraNota = int(terceiraNota)
+quartaNota = int(quartaNota)
+
+
+print(float((primeiraNota + segundaNota + terceiraNota + quartaNota)/4))
