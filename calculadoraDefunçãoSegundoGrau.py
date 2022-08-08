1fb4362235865be0f23d6e2dc8626cfa0779c7db
Marcelo914 <marcelosouza222244@gmail.com>
2022-08-08 15:52:36 -0300

    atualização função segundo grau

--- calculadoraDefunçãoSegundoGrau.py
+++ calculadoraDefunçãoSegundoGrau.py
@@ -6,14 +6,15 @@ from tkinter import Y
 A =int(input("valor de A: "))
 B = int(input("valor de B: "))
 C = int(input("valor de C: "))
-delta = (B**2) -4 * A * C
+B2 =B
+
+delta = B2**2 -(4 * A * C)
 if delta== 0:
-    X = -B/A*2
-    print("A raiz dessa equação é: ", X)
+    X = (-B + 0) //(A*2)
+    print("A raiz dessa equação é: ", int(X))
 if delta < 0:
     print ("Essa equação não possui raizes reais.")
 if delta > 0:
     x1 = -B + math.sqrt(delta)/2*A
     x2 = -B - math.sqrt(delta)/2*A
     print("As raizes da equação são:", int(x1), "e" ,  int(x2) )
-
