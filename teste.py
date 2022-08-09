b16771a16e95a7ebe81cada6af7657ab8875ae1b
Marcelo914 <marcelosouza222244@gmail.com>
2022-08-09 19:21:46 -0300

    A

--- teste.py
+++ teste.py
@@ -1,6 +1,4 @@
-if delta < 0:
-    print ("Essa equação não possui raizes reais.")
-if delta > 0:
-    x1 = (-B + math.sqrt(delta))/2*A
-    x2 = (-B - math.sqrt(delta))/2*A
-    print("As raizes da equação são:", float(x1), "e" ,  float(x2) )
+from re import A
+
+
+A
