cf4b809f1492395d440957f655970236d2d8353d
Marcelo914 <marcelosouza222244@gmail.com>
2022-08-15 15:23:10 -0300

    correção no fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,6 @@
 numero =int(input("digite o numero: "))
-if (numero%3) and (numero%5) == 0:
-    print("fizzbuzz")
+if (numero%3) == 0:
+    if (numero%5) == 0:
+        print("fizzbuzz")
 else:
     print(numero)
