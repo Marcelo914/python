72cb330a3557677194467fa13cb4606535875039
Marcelo914 <marcelosouza222244@gmail.com>
2022-08-15 15:18:48 -0300

    ordenação

---
+++ ordenacao.py
@@ -0,0 +1,11 @@
+numero1 = int(input("digite o numero 1: "))
+numero2 = int(input("digite o numero 2: "))
+numero3 = int(input("digite o numero 3: "))
+
+
+
+if numero1 < numero2 < numero3:
+    print("crescente")
+else:
+    print("não está em ordem crescente")
+    
